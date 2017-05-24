9d1614f37538e46cab1925071bcb6fe0b4557e88
vicxia <shf.xia@gmail.com>
2017-05-24 16:22:22 +0800

    OpenCV播放视频

--- LearnOpenCV3/LearnOpenCV3/VXOpenCVViewController.mm
+++ LearnOpenCV3/LearnOpenCV3/VXOpenCVViewController.mm
@@ -10,9 +10,14 @@
 #import "NSImage+OpenCV.h"
 #import "VXOpenCVViewController.h"
 
-@interface VXOpenCVViewController ()
+@interface VXOpenCVViewController () {
+    cv::VideoCapture _cap;
+    NSInteger _frameCnt;
+}
 
 @property (weak) IBOutlet NSImageView *imgView;
+@property (weak) IBOutlet NSView *bottomMaskView;
+@property (weak) IBOutlet NSSlider *slider;
 
 @end
 
@@ -20,7 +25,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+        
     NSString *path = [[NSBundle mainBundle] pathForResource:@"orgImage" ofType:@"jpg"];
     std::string imgPath = std::string([path UTF8String]);
     //读入的格式是BGR
@@ -30,6 +35,47 @@
     cv::cvtColor(inCvImg, outCvImg, cv::COLOR_BGR2RGB);
     //NSImage+OpenCV提供了NSImage<=>cv::Mat(RGB)的转换
     self.imgView.image = [NSImage imageWithCVMat:outCvImg];
+}
+
+- (void)viewWillAppear
+{
+    [super viewWillAppear];
+    self.bottomMaskView.layer.backgroundColor = [NSColor colorWithWhite:0 alpha:0.3].CGColor;
+}
+
+- (IBAction)onPlayBtnTap:(NSButton *)sender
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSString *videoPath = [[NSBundle mainBundle] pathForResource:@"orgVideo" ofType:@"mp4"];
+        if (_cap.isOpened()){
+            _cap.set(cv::CAP_PROP_POS_FRAMES, 0);
+        } else {
+            _cap.open(std::string([videoPath UTF8String]));
+            _frameCnt = _cap.get(cv::CAP_PROP_FRAME_COUNT);
+            int tmpw = (int) _cap.get(cv::CAP_PROP_FRAME_WIDTH);
+            int tmph = (int) _cap.get(cv::CAP_PROP_FRAME_HEIGHT);
+            NSLog(@"videoCapture: frameCnt = %ld; size=(%d, %d)", _frameCnt, tmpw, tmph);
+        }
+        NSInteger frameIdx = 0;
+        cv::Mat frame;
+        while(true) {
+            _cap >> frame;
+            frameIdx ++;
+            if (frame.empty()) break;
+            NSImage *image = [NSImage imageWithCVMat:frame];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                self.imgView.image = image;
+                self.slider.doubleValue = frameIdx * 100 / _frameCnt;
+            });
+            [NSThread sleepForTimeInterval:0.045];
+        }
+        NSLog(@"end, readFrame count = %ld", frameIdx);
+    });
+}
+
+- (IBAction)onSliderChange:(NSSlider *)sender
+{
+    
 }
 
 @end
